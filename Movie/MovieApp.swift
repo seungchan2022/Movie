73a3a7a06f4cab546efb19b9c9d9185d164cf9e2
승찬 <anstmdcks12@naver.com>
2023-07-12 15:08:35 +0900

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by 승찬 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
